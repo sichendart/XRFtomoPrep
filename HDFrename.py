bf723bbd1a41137d474f23e83eb8db8fd46413db
Si Chen <sichen@caper.xray.aps.anl.gov>
2018-07-21 11:35:42 -0500

    HDFrename.py

--- HDFrename.py
+++ HDFrename.py
@@ -10,11 +10,11 @@ import os
 import h5py
 import shutil
 
-#folder = '/home/beams/SICHEN/src/HDF_rename/test_data 2'
-#folder = '.'
-#path_name = '/home/x220/Dropbox/Si_Yifeng/work/test/'
-path_name = '/Users/sichen/src/XRFtomoPrep/test/'
-#path_name = '/home/x220/Dropbox/Si_Yifeng/work/'
+
+#path_name = '/Users/sichen/src/XRFtomoPrep/test/'
+path_name = '/mnt/micdata1/bnp/2018-2/Nowakowski_tomo/img.dat/'
+
+
 
 def h5_rename(path_name):
     new_path = path_name + 'h5.tomo/'
@@ -39,25 +39,3 @@ def h5_rename(path_name):
 if __name__ == '__main__':
     h5_rename(path_name)
     
-"""
-new_directory = folder+'h5.tomo/'
-
-for fname in os.listdir(folder):
-    if fname[-2:] == 'h5':
-       print(fname)
-       f = h5py.File(fname, 'r')
-       smplT = f['MAPS']['extra_pvs_as_csv'][8]
-       print(smplT)
-       print(type(smplT))
-       #pos = smplT.rfind(',')
-       #ang = round(float(smplT[pos+1:]))
-       ang = round(float(smplT.decode('UTF-8').split(',')[-1].strip()))
-       index = str(100+ang)
-       print(index)
-       #os.rename(fname, index+'_'+str(ang)+'_'+fname)
-       if not os.path.exists(new_directory):
-           os.mkdir(new_directory)
-       shutil.copy(fname, new_directory + index + '_' + str(ang) + fname)
-       
-       
-"""
